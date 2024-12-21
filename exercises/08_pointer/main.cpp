37c8a2767346c4c5f90118527c6dd46452708dca
jane017 <Jane_personal@163.com>
2024-12-21 21:35:32 +0800

    finish8

--- exercises/08_pointer/main.cpp
+++ exercises/08_pointer/main.cpp
@@ -5,6 +5,25 @@ bool is_fibonacci(int *ptr, int len, int stride) {
     ASSERT(len >= 3, "`len` should be at least 3");
     // TODO: 编写代码判断从 ptr 开始，每 stride 个元素取 1 个元素，组成长度为 n 的数列是否满足
     // arr[i + 2] = arr[i] + arr[i + 1]
+    
+    //int arr[len];
+    //int i;
+    // for(i=0;i<len;i++){
+    //     arr[i]= ptr[stride*i];
+    // }
+    // for(i=0;i<len-2;i++){
+    //     if (arr[i+2]!=arr[i+1]+arr[i]){
+    //         return false;
+    //     }
+        
+    // }
+
+    for(int i=0;i < len-2;i++){
+        if (ptr[stride*(i + 2)]!=ptr[stride*(i + 1)]+ptr[stride*i]){
+            return false;
+        }
+    }
+    
     return true;
 }
 
